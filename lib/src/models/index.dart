a71173bc82fe19c8607005714bf14a9f3826a99d
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-09-23 02:55:10 +0300

    add sort

--- lib/src/models/index.dart
+++ lib/src/models/index.dart
@@ -1,8 +1,8 @@
 export 'app_notification_model.dart';
 export 'campaign_model.dart';
+export 'category_model.dart';
 export 'developer_model.dart';
 export 'notification_model.dart';
 export 'special_agency_model.dart';
 export 'store_model.dart';
 export 'town_model.dart';
-export 'category_model.dart';
