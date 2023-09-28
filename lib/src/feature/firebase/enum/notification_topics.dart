f428b5ba441c6f7707255f3923e9e0710e50b3cd
vb10 <vbacik.10@gmail.com>
2023-09-28 03:31:58 +0300

    added raw value

--- lib/src/feature/firebase/enum/notification_topics.dart
+++ lib/src/feature/firebase/enum/notification_topics.dart
@@ -3,20 +3,18 @@ import 'package:json_annotation/json_annotation.dart';
 enum NotificationTopics {
   @JsonValue('/topics/everyone')
   toAll,
-
   single,
-
   @JsonValue('/topics/campaigns')
   forCampaign;
 
   String get rawValue {
     switch (this) {
       case NotificationTopics.toAll:
-        return '/topics/everyone';
+        return 'everyone';
       case NotificationTopics.single:
-        return '/topics/single';
+        return 'single';
       case NotificationTopics.forCampaign:
-        return '/topics/campaigns';
+        return 'campaigns';
     }
   }
 }
