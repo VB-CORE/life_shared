f3c303f5ce0285495ce2b0b9e3012fa66fc7b383
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2023-10-21 21:46:30 +0300

    add base drop down model inherit for genders

--- lib/src/feature/firebase/enum/gender.dart
+++ lib/src/feature/firebase/enum/gender.dart
@@ -1,10 +1,20 @@
 import 'package:json_annotation/json_annotation.dart';
+import 'package:life_shared/src/models/base_dropdown_model.dart';
 
-enum Genders {
+enum Genders implements BaseDropDownModel {
   @JsonValue('Erkek')
   male,
   @JsonValue('Kadın')
   female,
   @JsonValue('Diğer')
-  other,
+  other;
+
+  @override
+  String get displayName {
+    return switch (this) {
+      Genders.other => "Erkek/Kadın",
+      Genders.male => "Erkek",
+      Genders.female => "Kadın",
+    };
+  }
 }
